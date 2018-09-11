af3606e9859355ee3fa7cb9b739676db3f62134e
Hangyi <sweat0909@gmail.com>
2018-09-11 23:49:12 +0800

    Chapter 7: Large file structure

--- hello.py
+++ hello.py
@@ -6,6 +6,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -17,6 +19,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Role(db.Model):
     __tablename__ = 'roles'
---
+++ tests/test_basics.py
@@ -0,0 +1,23 @@
+import unittest
+from flask import current_app
+from app import create_app, db
+
+class BasicsTestCase(unittest.Testcase):
+    def setUp(self):
+        self.app = create_app('testing') # 创建一个测试环境，类似于运行中的程序。使用测试配置创建程序
+        self.app_context = self.app.app_context() 
+        self.app_context.push() # 激活上下文，确保能在测试中使用current_app
+        db.create_all() # 创建一个新的数据库
+
+    def setDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        """测试确保程序实例存在"""
+        self.assertFalse(current_app is None) 
+
+    def test_app_is_testing(self):
+        """测试确保程序在测试配置中运行"""
+        self.assertTrue(current_app.config['TESTING']) 
