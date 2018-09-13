4172f5d49b40d3100d991912ebc4dfbcfa724034
Hangyi <sweat0909@gmail.com>
2018-09-14 07:28:47 +0800

    Chapter 8: Email address changes (8h)

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,4 +1,6 @@
 import unittest
+import time
+from app import create_app, db
 from app.models import User
 
 class UserModelTestCase(unittest.TestCase):
@@ -65,10 +67,38 @@ class UserModelTestCase(unittest.TestCase):
         self.assertTrue(User.reset_password(token, 'dog'))
         self.assertTrue(u.verify_password('dog'))
 
-        def test_invalid_reset_token(self):
-            u = User(password='cat')
-            db.session.add(u)
-            db.session.commit()
-            token = u.generate_reset_token()
-            self.assertFalse(User.reset_password(token + 'a', 'horse'))
-            self.assertTrue(u.verify_password('cat'))
+    def test_invalid_reset_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_reset_token()
+        self.assertFalse(User.reset_password(token + 'a', 'horse'))
+        self.assertTrue(u.verify_password('cat'))
+
+    def test_valid_email_change_token(self):
+        u = User(email='john@example.com', password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_email_change_token('susan@example.org')
+        self.assertTrue(u.change_email(token))
+        self.assertTrue(u.email == 'susan@example.org')
+
+    def test_invalid_email_change_token(self):
+        u1 = User(email='john@example.com', password='cat')
+        u2 = User(email='susan@example.org', password='dog')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        token = u1.generate_email_change_token('david@example.net')
+        self.assertFalse(u2.change_email(token))
+        self.assertTrue(u2.email == 'susan@example.org')
+
+    def test_duplicate_email_change_token(self):
+        u1 = User(email='john@example.com', password='cat')
+        u2 = User(email='susan@example.org', password='dog')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        token = u2.generate_email_change_token('john@example.com')
+        self.assertFalse(u2.change_email(token))
+        self.assertTrue(u2.email == 'susan@example.org')
